db29a48ad7450352acb4ddbf2abd125134be8e08
Megan Mackey <megan.mackey24@gmail.com>
2020-12-02 01:56:37 -0600

    putting in bones

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,6 +1,12 @@
+// plays audio for both modes
 function playAudio(){
     let audio = new Audio('audio/wereportedthenews.mp3');
     audio.play();
 }
 
+// play audio when instant button is clicked
 instantReport.addEventListener('click', playAudio);
+
+// play audio at end of timer
+
+//switch active contents based on button clicked
