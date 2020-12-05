ae3f8ca4df7a38d8ef66817d2c2cfeff172b86c3
Megan Mackey <megan.mackey24@gmail.com>
2020-12-05 02:34:06 -0600

    adding buttons and number entry

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,12 +1,26 @@
+
+
+//switch active contents based on button clicked
+function instantReport () {
+    activecontent.innerHTML ='<button id="instantReportBtn">report!</button>';
+};
+
+instantModeSwitchBtn.addEventListener('click', instantReport);
+
+function timedReport () {
+    activecontent.innerHTML = '<form><input type="number"><button>start</button></form>';
+};
+
+timedModeSwitchBtn.addEventListener('click', timedReport);
+
 // plays audio for both modes
 function playAudio(){
     let audio = new Audio('audio/wereportedthenews.mp3');
     audio.play();
+    console.log('test');
 }
 
 // play audio when instant button is clicked
-instantReport.addEventListener('click', playAudio);
+instantReportBtn.addEventListener('click', playAudio);
 
 // play audio at end of timer
-
-//switch active contents based on button clicked
