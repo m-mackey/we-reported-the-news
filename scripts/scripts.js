6c28a0b737a5f3ba07f794718a98b4af2e47cfed
m-mackey <34154382+m-mackey@users.noreply.github.com>
2020-12-13 02:59:23 -0600

    update names

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,25 +1,23 @@
 // maybe i should rename things as its a little confusing
-let activeContent = document.getElementById('activecontent');
+let activeContent = document.getElementById('active-content');
 
-let instantModeSwitch = document.getElementById('instantModeSwitch');
+let instantModeSwitch = document.getElementById('instant-mode-switch');
 instantModeSwitch.addEventListener('click', instantReport);
 
 //switch active contents based on button clicked
 function instantReport () {
-    activeContent.innerHTML ='<button id="instantReportBtn">report!</button>';
+    activeContent.innerHTML ='<button id="instant-report-btn">report!</button>';
     // play audio when instant button is clicked
-    let instantReportBtn = document.getElementById('instantReportBtn');
+    let instantReportBtn = document.getElementById('instant-report-btn');
     instantReportBtn.addEventListener('click', playAudio);
     
 };
-
-
 
 function timedReport () {
     activeContent.innerHTML = '<form><input type="number"><button>start</button></form>';
 };
 
-let timedModeSwitch = document.getElementById('timedModeSwitch');
+let timedModeSwitch = document.getElementById('timed-mode-switch');
 timedModeSwitch.addEventListener('click', timedReport);
 
 // plays audio for both modes
@@ -30,7 +28,7 @@ function playAudio(){
 };
 
 // play audio when instant button is clicked
-let instantReportBtn = document.getElementById('instantReportBtn');
+let instantReportBtn = document.getElementById('instant-report-btn');
 instantReportBtn.addEventListener('click', playAudio);
 
 // play audio at end of timer
