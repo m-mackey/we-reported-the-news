c8e393101ce146315d3b7f8b366673943149c112
Megan Mackey <megan.mackey24@gmail.com>
2020-12-16 02:13:41 -0600

    setting up timer

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -10,7 +10,8 @@ function instantReport () {
     // play audio when instant button is clicked
     let instantReportBtn = document.getElementById('instant-report-btn');
     instantReportBtn.addEventListener('click', playAudio);
-    
+    clearInterval(countdown);
+    countdownDisplay.innerHTML= "";
 };
 
 function timedReport () {
@@ -32,3 +33,38 @@ let instantReportBtn = document.getElementById('instant-report-btn');
 instantReportBtn.addEventListener('click', playAudio);
 
 // play audio at end of timer
+
+
+// timer stuff
+let countdown; //wes said something about how if you dont want this in the glboal name space aroudn the 5 min mark but i didnt catch it
+const countdownDisplay = document.getElementById('countdown-display');
+
+function timer(seconds){
+    //gets current date
+    const now = Date.now();
+    //you have to times by 1000 because of how the seconds are by default. is the seconds here special or is it just what is passed because it isn't defined?
+    const then = now + seconds * 1000;
+    displayTimeLeft(seconds); //runs fn once immediately
+
+    //so this has an unnamed? arrow function in it. the first part b4 comma is the function that we are running, the second is how often it is run
+    countdown = setInterval(() => {
+        const secondsLeft = Math.round((then - Date.now()) / 1000);
+        // check if timer should stop
+        if(secondsLeft < 0){
+          clearInterval(countdown); //clearInterval is a method related to setInterval
+          return;
+        }
+        //display time left
+        displayTimeLeft(secondsLeft);
+    }, 1000);
+}
+
+function displayTimeLeft(seconds) {
+    const minutes = Math.floor(seconds/60);
+    const remainingSeconds = seconds % 60;
+    const display = `${minutes < 10 ? '0' : '' }${minutes}:${remainingSeconds < 10 ? '0' : ''}${remainingSeconds}`;
+    countdownDisplay.textContent = display;
+}
+
+// around 17 min
+
