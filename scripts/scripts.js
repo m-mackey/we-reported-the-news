4d29a4874bd4ae3f97f10824b45683dcf63d5ae1
Megan Mackey <megan.mackey24@gmail.com>
2021-09-09 02:55:45 -0500

    hover and focus added

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,21 +1,3 @@
-// maybe i should rename things as its a little confusing
-// let activeContent = document.getElementById('active-content');
-
-// let instantModeSwitch = document.getElementById('instant-mode-switch');
-// instantModeSwitch.addEventListener('click', instantReport);
-
-//switch active contents based on button clicked
-// function instantReport () {
-//     // activeContent.innerHTML ='<button id="instant-report-btn">report!</button>';
-//     // play audio when instant button is clicked
-//     let instantReportBtn = document.getElementById('instant-report-btn');
-//     instantReportBtn.addEventListener('click', playAudio);
-//     clearInterval(countdown);
-//     countdownDisplay.innerHTML= "";
-// };
-
-
-
 // plays audio for both modes
 function playAudio() {
     let audio = new Audio('audio/wereportedthenews.mp3');
